5baea94d3d6ecccd72dd913420c31af98a56478f
Joe Martella <joem@microsoft.com>
2020-10-04 20:57:09 -0700

    Updates

---
+++ precache-manifest.b3b96df54f6e040dcfc0a6cdcaef745a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07ac167863af60f653e1ccd5fd1c591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97a3b8ad50862ef011b",
+    "url": "/static/css/main.a5794a4d.chunk.css"
+  },
+  {
+    "revision": "0c2b6bc3624959d45388",
+    "url": "/static/js/2.993fb069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.993fb069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97a3b8ad50862ef011b",
+    "url": "/static/js/main.583f3342.chunk.js"
+  },
+  {
+    "revision": "813e4828f8064158049b",
+    "url": "/static/js/runtime-main.fd259064.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shortfuts-website/precache-manifest.80ce1c1cd382294f6b5b3b9e4e7d7471.js"
+  "/precache-manifest.b3b96df54f6e040dcfc0a6cdcaef745a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shortfuts-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
