2a659a56c5e90076c474b960f663bc98812a2246
Joe Martella <joem@microsoft.com>
2020-10-16 01:59:46 -0700

    removes unused func

--- src/App.js
+++ src/App.js
@@ -33,33 +33,6 @@ function App() {
   );
 }
 
-const buyShortfutsMonthlySubscription = async (event) => {
-  // Get Stripe.js instance
-  const stripe = await stripePromise;
-
-  // Call your backend to create the Checkout Session
-  const response = await fetch(
-    "https://shortfuts-server.azurewebsites.net/purchase-monthly",
-    { method: "POST" }
-  );
-  // const response = await fetch("http://localhost:3000/purchase-monthly", {
-  //   method: "POST",
-  // });
-
-  const session = await response.json();
-
-  // When the customer clicks on the button, redirect them to Checkout.
-  const result = await stripe.redirectToCheckout({
-    sessionId: session.id,
-  });
-
-  if (result.error) {
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `result.error.message`.
-  }
-};
-
 const buyShortfutsForFifa21 = async (url) => {
   const confirmResponse = window.confirm(
     "If you choose to purchase shortfuts premium with PayPal, you must make sure that the contact email you provide matches your Chrome Web Store email.\n\nIf you pay with credit card, you must make sure that the contact email you provide matches your Chrome Web Store email.\n\nIf it doesn't match, your shortfuts premium will not work and you will not be refunded."
